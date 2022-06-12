2511567a40ebb3806c74f3626a569df7ebebe344
emisir <82768119+emisir@users.noreply.github.com>
2022-06-12 12:52:08 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-      System.out.println("Hoffentlich hat ihnen Unsere Präsentation gefallen");
+      System.out.println("Hallo, Welt");
    }
 }
